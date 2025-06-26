f885197d9a09069113c0ebec6160011bf35f233f
Melyssa-Ingabe <m.mbayire@alustudent.com>
2025-06-26 04:15:40 -0700

    take0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return None
-    return [[num * num for num in row] for row in matrix]
+    return [[x ** 2 for x in row] for row in matrix]
